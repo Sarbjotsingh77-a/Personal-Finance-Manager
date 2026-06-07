11c23fbad59fe7a6e3917792c1f1a8798b99fe4e
Sarbjotsingh77-a <kherarajindersingh94@gmail.com>
2026-06-08 02:37:38 +0530

    Create expense.py

---
+++ src/expense.py
@@ -0,0 +1,22 @@
+class Expense:
+    def __init__(self, amount, category, date, description):
+        self.amount = float(amount)
+        self.category = category
+        self.date = date
+        self.description = description
+
+    def to_list(self):
+        return [
+            self.date,
+            self.category,
+            self.amount,
+            self.description
+        ]
+
+    def __str__(self):
+        return (
+            f"{self.date} | "
+            f"{self.category} | "
+            f"₹{self.amount:.2f} | "
+            f"{self.description}"
+        )
